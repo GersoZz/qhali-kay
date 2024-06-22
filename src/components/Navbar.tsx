2fd95fdb4971c26ed3c2f124b0d8f7bdd1d17902
GersoZz <37023982+GersoZz@users.noreply.github.com>
2024-06-22 12:59:31 -0500

    feat: add tailwindCSS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,11 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-teal-400 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-lg">Navbar</div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
